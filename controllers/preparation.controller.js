7ee628e60b5bd9eb7f71ea547561813e63390ebe
Antoine Ricci <aricci95@gmail.com>
2020-09-02 20:14:40 +0200

    add preparation filter

--- controllers/preparation.controller.js
+++ controllers/preparation.controller.js
@@ -1,9 +1,12 @@
 var PreparationService = require('../services/preparation.service')
 var User = require('../models/user.model')
+const url = require('url');
 const globals = require('../consts')
 
 exports.listAction = async function (req, res, next) {
     const currentUser = await User.findOne({ email: req.headers.email, password: req.headers.token })
+
+    let { filter } = url.parse(req.url,true).query
 
     if (!currentUser || currentUser.role > globals.ROLE_USER) {
         res.status(403).send({ message: 'Forbidden' });
@@ -11,7 +14,7 @@ exports.listAction = async function (req, res, next) {
     }
 
     try {
-        var tables = await PreparationService.getAll(currentUser)
+        var tables = await PreparationService.getAll(currentUser, filter)
         return res.status(200).json(tables);
     } catch (e) {
         return res.status(400).json({ status: 400, message: e.message });
--- services/preparation.service.js
+++ services/preparation.service.js
@@ -1,19 +1,26 @@
 var Table = require('../models/table.model');
+const globals = require('../consts');
 
-const globals = require('../consts')
+exports.getAll = async function (currentUser, filter = null) {
+    let deleteIds = []
 
-exports.getAll = async function (currentUser) {
-    let tables = await Table.find({ restaurantId: currentUser.restaurantId }).select("-restaurantId").sort({ createdAt: 'asc' })
+    let removeTypes = []
+
+    if (filter === 'food') {
+        removeTypes = ['Boisson']
+    } else if (filter === 'bar') {
+        removeTypes = ['Starter', 'Plat', 'Dessert']
+    }
+
+    tables = await Table.find({ restaurantId: currentUser.restaurantId }).select("-restaurantId").sort({ createdAt: 'asc' })
 
     for (var key in tables) {
         for (var subKey in tables[key].foods) {
-            if (tables[key].foods[subKey].status != globals.PREPARATION_STATUS_PREPARATION) {
+            if (tables[key].foods[subKey].status != globals.PREPARATION_STATUS_PREPARATION || removeTypes.includes(tables[key].foods[subKey].type)) {
                 tables[key].foods.splice(subKey, 1)
             }
         }
     }
-
-    let deleteIds = []
 
     for (var key in tables) {
         if (!tables[key].foods || tables[key].foods.length === 0) {
@@ -29,6 +36,5 @@ exports.getAll = async function (currentUser) {
             }
         }
     }
-
     return tables;
 }
